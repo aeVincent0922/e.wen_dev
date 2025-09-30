26b3c08856ac268972c0c45ba6ee31f10a7a7872
vincent <yoyo12825@gmail.com>
2025-09-30 14:55:08 +0800

    引入英文字體、修改首頁標題動畫

--- src/components/Section_1.vue
+++ src/components/Section_1.vue
@@ -30,7 +30,7 @@ onMounted(() => {
   // });
   const st_1 = SplitText.create(".split-title", {
     type: "chars, words",
-    mask: "chars",
+    mask: "words",
     autoSplit: true,
   });
   const st_2 = SplitText.create(".vanishing-text", {
@@ -39,19 +39,68 @@ onMounted(() => {
     autoSplit: true,
   });
 
-  tl.from(st_1.chars, {
-    duration: 0.5,
-    y: -50,
-    autoAlpha: 0,
-    stagger: 0.1,
-  })
-    .to(st_2.words, {
+  tl.set(".first-title", { autoAlpha: 0, yPercent: 100 })
+    .set(".vanishing-text", { autoAlpha: 0 })
+    .to(".first-title", {
       duration: 1,
-      opacity: 0,
+      yPercent: 0,
+      autoAlpha: 1,
+      stagger: 0.1,
       ease: "power2.out",
     })
-    .set(".e_1_comma", { display: "block" })
-    .to(".e_1_comma", { duration: 0.8, ease: "bounce.out", opacity: 1 });
+    .to(".vanishing-text", {
+      duration: 1,
+      autoAlpha: 1,
+      stagger: 0.6,
+      ease: "power2.out",
+      delay: 0.8,
+    })
+    .set(".e_1_comma", { display: "block", autoAlpha: 0 })
+    .to(".e_1_comma", { duration: 0.8, ease: "bounce.out", autoAlpha: 1 })
+    .to(".st_1", {
+      scrollTrigger: {
+        trigger: ".st_1",
+        start: "bottom 15%",
+        end: "bottom top",
+        scrub: true,
+      },
+      xPercent: -100,
+      duration: 2,
+      ease: "power2.in",
+    })
+    .to(".st_3", {
+      scrollTrigger: {
+        trigger: ".st_3",
+        start: "top 20%",
+        end: "top top",
+        scrub: true,
+      },
+      xPercent: -200,
+      duration: 3,
+      ease: "power2.in",
+    })
+    .to(".st_2", {
+      scrollTrigger: {
+        trigger: ".st_2",
+        start: "bottom 20%",
+        end: "bottom top",
+        scrub: true,
+      },
+      xPercent: 200,
+      duration: 2,
+      ease: "power2.in",
+    })
+    .to(".st_4", {
+      scrollTrigger: {
+        trigger: ".st_4",
+        start: "top 30%",
+        end: "top top",
+        scrub: true,
+      },
+      xPercent: 300,
+      duration: 5,
+      ease: "power2.in",
+    });
 });
 </script>
 <template>
@@ -60,23 +109,23 @@ onMounted(() => {
     <!-- <div class="horizontal-line"></div> -->
     <!-- 首頁標題動畫 -->
     <div
-      class="container absolute text-7xl top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-black leading-23 flex flex-col item-center pl-15"
+      class="container absolute text-6xl leading-20 top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-black flex flex-col item-center pl-5"
     >
-      <div class="w-fit split-title flex e_1">
-        e
-        <div class="opacity-0 e_1_comma hidden">.</div>
+      <div class="w-fit split-title flex st_1">
+        <div class="first-title">e</div>
+        <div class="e_1_comma hidden">.</div>
         <div class="vanishing-text">very</div>
       </div>
-      <div class="w-fit split-title flex w">
-        w
+      <div class="w-fit split-title flex st_2">
+        <div class="first-title">w</div>
         <div class="vanishing-text">aning</div>
       </div>
-      <div class="w-fit split-title flex e_2">
-        e
+      <div class="w-fit split-title flex st_3">
+        <div class="first-title">e</div>
         <div class="vanishing-text">ffort</div>
       </div>
-      <div class="w-fit split-title flex n">
-        n
+      <div class="w-fit split-title flex st_4">
+        <div class="first-title">n</div>
         <div class="vanishing-text">udges</div>
       </div>
     </div>
